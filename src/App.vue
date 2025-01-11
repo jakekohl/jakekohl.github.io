a1db0253374abcd40b17c365e12493f1562cf986
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-01-11 15:28:47 -0600

    pivoting towards just learning FrontEnd

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,16 @@
 <script setup>
-import { useDark, useToggle } from "@vueuse/core";
 
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
+
 </script>
 
 <template>
   <div id="app">
     <Nav />
-    <button data-entity="toggle-dark-mode" @click="toggleDark()">
-    Toggle Dark Mode
-    </button>
+    
     <router-view/>
   </div>
 </template>
-<script>
-import Nav from './components/partials/nav.vue';
 
-export default {
-  name: 'app',
-  components: {
-    Nav
-  }  
-}
-</script>
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-.dark {
-  background: #16171d;
-  color: #fff;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 // Create the app instance
 const app = createApp(App);
 
 // Use the router
 app.use(router);
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+      preset: Aura,
+      options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+      }
+  }
+});
 
 // Mount the app
 app.mount('#app');
