322185eb49656d03081ff14602fe898387f1a439
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-10-03 15:04:29 -0500

    feature: added linting configuration + npm command updates

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'kz76nf',
+  retries: { "runMode": 1, "openMode": 0 },
+  defaultBrowser: 'electron',
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173',
